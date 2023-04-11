68009f4644c9f0e5d11844ba1371bfadd1b79c1e
Luis Antonio Flores Castro <55300309+LuisFC7@users.noreply.github.com>
2023-04-10 23:02:23 -0600

    Add files via upload

--- Keywords.js
+++ Keywords.js
@@ -1,47 +1,31 @@
-// function detection(){
-//     //First off all
-//     //Divide de string in tokens by blank spaces
-//     const cone="marketing, intern, assistance;"//This is one case
-//     const sone="Administrative Assistant and Accounting, Fresher";
-//     const firtsexpression=/[\w]+[\s]*[\w]+/;//Here we detect just the words without spaces at the beginning or final
-//     let container=[];
-//     //First count number 
-//     if (firtsexpression.test(cone)) {
-//         let aux=cone;
-//         console.log(cone);
-//       } else {
-//         console.log('Match not found.');
-//       }
-
-// }
-
-// detection();
 function detection(){
     const cone="marketing, intern, assistance;";
-    const sone="Administrative Assistant and Accounting, Fresher";
-    const regex = /([\w]+[\s]*[\w]+[\s\w]*)+/g;
+    const sone="Administrative Assistant and Accounting, Fresher, No de.Js, .net core, BSA - SAP SS/PM";
+    // const sone="No de.Js, .net core";
+    // const regex = /([\w]+[\s]*[\w]+[\s\w]*)+/g;
+    const regex = /([\.]*[\w]+[\s]*[\-\.\s\w]*[\/]*[\w]+[\s\w]*)+/g;
+    const regex2= /^[\.][\w]+[\s]*[\w]*/g;//To find dot
     // /[\w]+[\s]*[\w]+/g;
     let container=[];
   
-    const matches = cone.match(regex); // Buscar coincidencias usando el método match
-  
-    // if (matches) {
-    //   console.log(matches[0]); // Imprimir la primera coincidencia encontrada
-    // } else {
-    //   console.log('Match not found.');
-    // }
+    //Delete special characters
     //Clean pattern detected
-    let texto = "  Hola Mundo  ";
-    let textoLimpio = texto.replace(/\s+/g, '');
-
     let match;
     while ((match = regex.exec(sone)) !== null) {
-        let depure = match[0].replace(/\s+/g, '');
-        container.push("#"+depure);
+        let depure;
+        //Condition for dot
+        if(match[0].match(regex2)){
+            depure = match[0].replace(/([\.]+([\-]*[\/]*))/g, '');
+            depure = "#dot"+depure;
+            depure = depure.replace(/[\s]*/g, '');
+            container.push(depure);
+        }else{
+            depure = match[0].replace(/([\.]*[\s]*)*/g, '');
+            depure = depure.replace(/(([\-]*[\/]*))/g, '');
+            container.push("#"+depure);
+        }
     }
   
   console.log(container);
 }
-
-  
 detection();
